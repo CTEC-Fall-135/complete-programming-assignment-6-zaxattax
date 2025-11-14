3f1ee77cfa7f81138913af43a9a3df5419ef5d0b
Zachery Alexander <zach8528alex@gmail.com>
2025-11-14 10:12:51 -0800

    Commit

---
+++ ConfigDemo/FactoryModel/Program.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.Data.Common;
+using Microsoft.Data.SqlClient;
+
+namespace FactoryModel
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n Factory Model Example");
+
+            var (provider, connectionString) = GetProviderFromConfiguration();
+            Console.WriteLine($"Provider: {provider}");
+            Console.WriteLine($"ConnectionString: {connectionString}");
+
+            DbProviderFactory factory = GetDbProviderFactory(provider);
+
+            using (DbConnection connection = factory.CreateConnection())
+            {
+                Console.WriteLine($"Connection Type: {connection.GetType().Name}");
+
+                connection.ConnectionString = connectionString;
+                connection.Open();
+
+                DbCommand command = factory.CreateCommand();
+                Console.WriteLine($"Command Type: {command.GetType().Name}");
+
+                command.Connection = connection;
+
+                command.CommandText = "Select i.Id, m.Name From Inventory i inner join Makes m on m.Id = i.MakeId";
+                using (DbDataReader reader = command.ExecuteReader())
+                {
+                    Console.WriteLine($"Id \t Your data reader object is a : {reader.GetType().Name}");
+                    Console.WriteLine("\n\t *** Current Inventory ***");
+
+                    while (reader.Read())
+                    {
+                        Console.WriteLine($"\t-> Car # {reader["Id"]} is a {reader["Name"]}");
+                    }
+                }
+            }
+        } // Main method closing brace
+
+        static DbProviderFactory GetDbProviderFactory(string provider)
+        {
+            if (provider == "SqlServer")
+            {
+                return SqlClientFactory.Instance;
+            }
+            else return null;
+        }
+
+        static (string Provider, string ConnectionString) GetProviderFromConfiguration()
+        {
+            IConfiguration configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", true, true)
+                .Build();
+
+            var providerName = configuration["ProviderName"];
+            var connectionString = configuration[$"{providerName}:ConnectionString"];
+            return (providerName, connectionString);
+        }
+    }
+}
